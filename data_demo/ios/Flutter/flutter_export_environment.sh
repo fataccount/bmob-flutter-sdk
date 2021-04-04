f3366f93932d354ab21b1273e6059719eac7674c
shaomaicheng <devchenglei@gmail.com>
2021-04-04 17:37:40 +0800

    配置改成本地代码

--- data_demo/ios/Flutter/flutter_export_environment.sh
+++ data_demo/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/zhangchaozhou/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zhangchaozhou/Documents/BmobGithub/bmob-flutter-sdk/data_demo"
+export "FLUTTER_ROOT=/Users/chenglei01/flutters/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chenglei01/private/flutter_plugins/bmob-flutter-sdk/data_demo"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/zhangchaozhou/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_BUILD_NAME=0.0.11"
 export "FLUTTER_BUILD_NUMBER=0.0.11"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
